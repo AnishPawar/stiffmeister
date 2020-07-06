1180a0acceb2fefc227b342f737e6f2ef597c78e
AnishPawar <anish.pawar@somaiya.edu>
2020-07-06 19:45:53 +0530

    [Removing ROS Directory]

--- ROS/catkin_ws/build/messaging/catkin_generated/installspace/messaging-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${messaging_DIR}/.." "msg" messaging_MSG_INCLUDE_DIRS UNIQUE)
-set(messaging_MSG_DEPENDENCIES std_msgs)
--- ROS/catkin_ws/devel/share/messaging/cmake/messaging-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(messaging_MSG_INCLUDE_DIRS "/home/anishpawar/Git/stiffmeister/ROS/catkin_ws/src/messaging/msg")
-set(messaging_MSG_DEPENDENCIES std_msgs)
