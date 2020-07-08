8b63742d918aeb57f068ad05e13e4432698b2dea
AnishPawar <56557214+AnishPawar@users.noreply.github.com>
2020-07-09 00:22:44 +0530

    []

--- IP/Learning/Distance_Measurement/disparity_map.py
+++ IP/Learning/Distance_Measurement/disparity_map.py
@@ -9,9 +9,15 @@ Left_Gray = cv2.cvtColor(Left,cv2.COLOR_BGR2GRAY)
 Right = cv2.imread('Right_Cam.jpg')
 Right_Gray = cv2.cvtColor(Right,cv2.COLOR_BGR2GRAY)
 
-stereo = cv2.StereoSGBM_create(minDisparity=35,numDisparities=103,blockSize=6,P1=246 ,P2 =255,disp12MaxDiff=8,preFilterCap=0,uniquenessRatio=0,speckleWindowSize=0,speckleRange=0)
-disparity = stereo.compute(Left_Gray,Right_Gray)
-plt.imshow(disparity,'gray')
+stereo_SGBM = cv2.StereoSGBM_create(minDisparity=35,numDisparities=103,blockSize=6,P1=246 ,P2 =255,disp12MaxDiff=8,preFilterCap=0,uniquenessRatio=0,speckleWindowSize=0,speckleRange=0)
+disparity_SGBM = stereo_SGBM.compute(Left,Right)
+
+stereo_BM = cv2.StereoBM_create(numDisparities=16,blockSize=5)
+disparity_BM = stereo_BM.compute(Left_Gray,Right_Gray)
+
+plt.imshow(disparity_SGBM,'gray')
+plt.show()
+plt.imshow(disparity_BM,'gray')
 plt.show()
 
     # Golden_values  
